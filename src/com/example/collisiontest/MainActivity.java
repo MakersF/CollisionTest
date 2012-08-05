e98fed19898e37d5a9b305f243c46aa00a5ebf97
Makers_F <makers.f@gmail.com>
2012-08-05 18:03:12 +0200

    Example now is working correctly

--- src/com/example/collisiontest/MainActivity.java
+++ src/com/example/collisiontest/MainActivity.java
@@ -5,10 +5,9 @@ import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
-import org.andengine.entity.shape.RectangularShape;
-import org.andengine.entity.shape.Shape;
 import org.andengine.entity.util.FPSLogger;
 import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.TextureManager;
@@ -21,12 +20,9 @@ import org.andengine.ui.activity.BaseGameActivity;
 import android.util.Log;
 
 import com.makersf.andengine.extension.collisions.CollisionLogger;
-import com.makersf.andengine.extension.collisions.entity.sprite.PixelPerfectAnimatedSprite;
 import com.makersf.andengine.extension.collisions.entity.sprite.PixelPerfectSprite;
 import com.makersf.andengine.extension.collisions.opengl.texture.region.PixelPerfectTextureRegion;
 import com.makersf.andengine.extension.collisions.opengl.texture.region.PixelPerfectTextureRegionFactory;
-import com.makersf.andengine.extension.collisions.opengl.texture.region.PixelPerfectTiledTextureRegion;
-import com.makersf.andengine.extension.collisions.pixelperfect.masks.MaskUtils;
 
 public class MainActivity extends BaseGameActivity {
 	
@@ -36,16 +32,13 @@ public class MainActivity extends BaseGameActivity {
 	
 	private Camera mCamera;
 	private Scene mScene;
-	private BitmapTextureAtlas triangleTexture;
     private BitmapTextureAtlas diamondTexture;
-    private PixelPerfectTiledTextureRegion triangleRegion;
-    private PixelPerfectTextureRegion diamond100Region;
-    private PixelPerfectTextureRegion diamond32Region;
     
     private PixelPerfectTextureRegion starRegion;
     
     private CollisionLogger logger;
-    private Shape[] mShapes;
+    private PixelPerfectSprite star1;
+    private PixelPerfectSprite star2;
     
 	@Override
 	public EngineOptions onCreateEngineOptions() {
@@ -63,14 +56,9 @@ public class MainActivity extends BaseGameActivity {
 		PixelPerfectTextureRegionFactory.setAssetBasePath("gfx/");
         BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
 
-        this.triangleTexture = new BitmapTextureAtlas(textureManager, 2048, 128, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
         this.diamondTexture = new BitmapTextureAtlas(textureManager, 1024, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-        this.triangleRegion = PixelPerfectTextureRegionFactory.createTiledFromAsset(triangleTexture, this, "spinning-triangle.png", 0, 0, 20, 1, ALPHA_THERSHOLD);
-        this.diamond100Region = PixelPerfectTextureRegionFactory.createFromAsset(diamondTexture, this, "diamond-100.png", 0, 0, ALPHA_THERSHOLD);
-        this.diamond32Region = PixelPerfectTextureRegionFactory.createFromAsset(diamondTexture, this, "diamond-32.png", 102, 0, ALPHA_THERSHOLD);
         this.starRegion = PixelPerfectTextureRegionFactory.createFromAsset(diamondTexture, this, "star.png", 200, 0, ALPHA_THERSHOLD);
 
-        this.triangleTexture.load();
         this.diamondTexture.load();
         
         pOnCreateResourcesCallback.onCreateResourcesFinished();
@@ -87,7 +75,7 @@ public class MainActivity extends BaseGameActivity {
 		
 		final VertexBufferObjectManager VBOmanager = this.getVertexBufferObjectManager();
 		
-		final PixelPerfectSprite diamond100 = new PixelPerfectSprite(400, 100, diamond100Region, VBOmanager){
+		star2 = new PixelPerfectSprite(150, 50, starRegion, VBOmanager){
 			
 			boolean mGrabbed = false;
             
@@ -100,13 +88,6 @@ public class MainActivity extends BaseGameActivity {
                     case TouchEvent.ACTION_MOVE:
                         if(this.mGrabbed) {
                             this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2, pSceneTouchEvent.getY() - this.getHeight() / 2);
-                            for (Shape B : mShapes)
-        						if(this != B) {
-        							logger.startCollisionCheck();
-        							boolean collisionResult = this.collidesWith(B);
-        							logger.endCollisionCheck(collisionResult);
-        						}
-                            logger.printStatistics(true);
                         }
                         break;
                     case TouchEvent.ACTION_UP:
@@ -117,66 +98,54 @@ public class MainActivity extends BaseGameActivity {
                 }
                 return true;
             }
-            
-            @Override
-			public boolean collidesWith(RectangularShape pOtherShape) {
-				if(super.collidesWith(pOtherShape)) {
-					Log.i("COLLIDING_DIAMOND", "COLLISION!!!!");
-					return true;
-				}
-				Log.i("COLLIDING_DIAMOND", "NO COLLISION!!!!");
-				return false;
-			}
         };
-        final PixelPerfectSprite diamond32 = new PixelPerfectSprite(300,100, diamond32Region, VBOmanager);
-        final PixelPerfectSprite star = new PixelPerfectSprite(0, 50, starRegion, VBOmanager);
-        
-        mScene.attachChild(diamond100);
-        mScene.registerTouchArea(diamond100);
-        mScene.attachChild(diamond32);
-        mScene.attachChild(star);
-        
-        MaskUtils.writeMaskToSDCard(diamond100.getPixelPerfectMask(), "/collisiontest", "diamond100");
-        MaskUtils.writeMaskToSDCard(diamond32.getPixelPerfectMask(), "/collisiontest", "diamond32");
-        MaskUtils.writeMaskToSDCard(star.getPixelPerfectMask(), "/collisiontest", "star");
-    	
-        final PixelPerfectAnimatedSprite spinningTriangle = new PixelPerfectAnimatedSprite(150, 200, triangleRegion, VBOmanager);
-        spinningTriangle.setCurrentTileIndex(9);
-        spinningTriangle.animate(200);
-        
-        for(int i =0; i< spinningTriangle.getTileCount(); i++) {
-        	MaskUtils.writeMaskToSDCard(spinningTriangle.getPixelMask(i), "/collisiontest", "triangle" + i);
-        }
-        
-        mScene.attachChild(spinningTriangle);
-        
-        mShapes = new Shape[]{diamond100, diamond32, star, spinningTriangle};
-        
-        pOnCreateSceneCallback.onCreateSceneFinished(mScene);
+       star1 = new PixelPerfectSprite(0, 50, starRegion, VBOmanager);
+       Rectangle roatator = new Rectangle(400, 270, 80, 50, VBOmanager){
+
+		@Override
+		public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
+				float pTouchAreaLocalX, float pTouchAreaLocalY) {
+			star2.setRotation(star2.getRotation() + 1);
+			return true;
+		}
+    	   
+       };
+       
+       mScene.attachChild(star1);
+       mScene.attachChild(star2);
+       mScene.registerTouchArea(star2);
+       mScene.registerTouchArea(roatator);
+       
+       pOnCreateSceneCallback.onCreateSceneFinished(mScene);
 		
 	}
 
 	@Override
 	public void onPopulateScene(Scene pScene,
 			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
-		pScene.registerUpdateHandler(new IUpdateHandler() {
-			
+		mScene.registerUpdateHandler(new IUpdateHandler() {
+			float time;
 			@Override
 			public void reset() {
+				// TODO Auto-generated method stub
 				
 			}
 			
 			@Override
 			public void onUpdate(float pSecondsElapsed) {
-				/*for(Shape A : mShapes) 
-					for (Shape B : mShapes)
-						if(A != B) {
-							logger.startCollisionCheck();
-							boolean collisionResult = A.collidesWith(B);
-							logger.endCollisionCheck(collisionResult);
-						}
-				//logger.printStatistics(true);
-				*/
+				time += pSecondsElapsed;
+				
+				if(time <1)
+					return;
+				time = 0;
+				
+				logger.startCollisionCheck();
+				boolean collisionResult = star1.collidesWith(star2);
+				logger.endCollisionCheck(collisionResult);
+				if(collisionResult) {
+					Log.i("STAR LOGGER", "STAR1 COLLIDES WITH STAR2");
+				}
+				logger.printStatistics(false);
 			}
 		});
 	
